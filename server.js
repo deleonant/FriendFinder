809fca41943df327f8283da89a8a5fc524439cbe
Antonio <deleonant@gmail.com>
2016-09-24 22:39:35 -0400

    Friend Finder

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/data/routing/api-routes.js')(app); 
-require('./app/data/routing/html-routes.js')(app);
+require('./app/routing/api-routes.js')(app); 
+require('./app/routing/html-routes.js')(app);
 
 
 
